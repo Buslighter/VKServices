7f914e948cad4af7cdd97cdf0d47b5620e20cd9c
Buslighter <gelbcka@gmail.com>
2022-07-14 20:10:54 +0300

    Initial Commit

---
+++ VKServices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKServices
+//
+//  Created by gleba on 14.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
